b37a2338426f31f09a6e22d7eba640c2ce7c98c9
dbajj <daniel.bajj@gmail.com>
2018-02-10 14:20:01 -0800

    Initial commit

---
+++ Assets/CubeEventTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CubeEventTrigger : EventTrigger {
+
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        var player = GameObject.Find("Player");
+
+        player.transform.position = new Vector3(gameObject.transform.position.x, player.transform.position.y, gameObject.transform.position.z);
+    }
+
+}
---
+++ Assets/TransformScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    }
+
+    public void MoveABit()
+    {
+        transform.Translate(Vector3.forward);
+    }
+}
